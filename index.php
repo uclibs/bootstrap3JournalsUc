bd08a2c85883c93cc4a9efb0315b0f5f8656e9aa
crowesn <crowesn@gmail.com>
2023-12-12 16:01:36 -0500

    update for ojs v3.4

--- index.php
+++ index.php
@@ -11,13 +11,11 @@
  * Copyright (c) 2003-2016 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
- * @ingroup plugins_themes_defaultChild
- * @brief Wrapper for default child theme plugin.
+ * @ingroup plugins_themes_boostrap3JournalsUc
+ * @brief Wrapper for bootstrap3 child theme plugin.
  *
  */
 
-require_once('BootstrapThreeJournalsUcPlugin.inc.php');
-
-return new BootstrapThreeJournalsUcPlugin();
+return new \APP\plugins\themes\bootstrap3JournalsUc\BootstrapThreeJournalsUcPlugin();
 
 ?>
